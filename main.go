1a489ae96fe8bc33b7efb8b64541f7b553f69996
Jonas Seiler <Me@jonasseiler.de>
2024-12-01 22:44:25 +0100

    add pretty printing for all solutions

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+
+	day1 "git.jonasseiler.de/Jonas/AdventOfCode2024/Day1"
+)
+
+type solution struct {
+	name          string
+	part1function func() string
+	part2function func() string
+}
+
 func main() {
 
+	solutions := []solution{
+		{"Historian Hysteria", day1.TotalDistance, day1.SimilarityScore},
+	}
+
+	for i, s := range solutions {
+		fmt.Println("Day", i+1, ":", s.name)
+
+		fmt.Println("    Part1:", s.part1function())
+		//fmt.Println("")
+
+		fmt.Println("    Part2:", s.part2function())
+		fmt.Println("")
+
+	}
 }
