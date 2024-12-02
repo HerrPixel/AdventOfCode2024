71c194498ef965c24b878a806cce764141cd76d3
Jonas Seiler <Me@jonasseiler.de>
2024-12-02 08:14:47 +0100

    add Day2

---
+++ Day2/day2.go
@@ -0,0 +1,110 @@
+package day2
+
+import (
+	"strconv"
+	"strings"
+
+	"git.jonasseiler.de/Jonas/AdventOfCode2024/Tools"
+)
+
+func parseInput() [][]int {
+	reports := make([][]int, 0)
+
+	for _, s := range Tools.ReadByLines("./Day2/input.txt") {
+		levels := strings.Split(s, " ")
+
+		reports = append(reports, make([]int, 0))
+		for _, levelString := range levels {
+			level, _ := strconv.Atoi(levelString)
+			reports[len(reports)-1] = append(reports[len(reports)-1], level)
+		}
+	}
+
+	return reports
+}
+
+func SafeReports() string {
+	reports := parseInput()
+
+	safeReports := 0
+
+	for _, report := range reports {
+		if hasMinAndMaxDifference(report, 1, 3) && (isIncreasing(report) || isDecreasing(report)) {
+			safeReports++
+
+		}
+	}
+
+	return strconv.Itoa(safeReports)
+}
+
+func DampenedReports() string {
+	reports := parseInput()
+
+	safeReports := 0
+
+	for _, report := range reports {
+		if hasMinAndMaxDifference(report, 1, 3) && (isIncreasing(report) || isDecreasing(report)) {
+			safeReports++
+
+		} else {
+			for i := range report {
+				dampenedReport := remove(report, i)
+
+				if hasMinAndMaxDifference(dampenedReport, 1, 3) && (isIncreasing(dampenedReport) || isDecreasing(dampenedReport)) {
+					safeReports++
+					break
+				}
+
+			}
+		}
+	}
+
+	return strconv.Itoa(safeReports)
+}
+
+func isIncreasing(l []int) bool {
+	curr := l[0]
+
+	for _, next := range l {
+		if next < curr {
+			return false
+		}
+		curr = next
+	}
+	return true
+}
+
+func isDecreasing(l []int) bool {
+	curr := l[0]
+
+	for _, next := range l {
+		if next > curr {
+			return false
+		}
+		curr = next
+	}
+	return true
+}
+
+func hasMinAndMaxDifference(l []int, min int, max int) bool {
+	curr := l[0]
+
+	for i, next := range l {
+		if i == 0 {
+			continue
+		}
+		if max < Tools.Abs(curr-next) || Tools.Abs(curr-next) < min {
+			return false
+		}
+		curr = next
+	}
+	return true
+}
+
+func remove(l []int, i int) []int {
+	removedList := make([]int, i, len(l)-1)
+	_ = copy(removedList, l[:i])
+	removedList = append(removedList, l[i+1:]...)
+	return removedList
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	day1 "git.jonasseiler.de/Jonas/AdventOfCode2024/Day1"
+	day2 "git.jonasseiler.de/Jonas/AdventOfCode2024/Day2"
 )
 
 type solution struct {
@@ -16,13 +17,13 @@ func main() {
 
 	solutions := []solution{
 		{"Historian Hysteria", day1.TotalDistance, day1.SimilarityScore},
+		{"Red-Nosed Reports", day2.SafeReports, day2.DampenedReports},
 	}
 
 	for i, s := range solutions {
 		fmt.Println("Day", i+1, ":", s.name)
 
 		fmt.Println("    Part1:", s.part1function())
-		//fmt.Println("")
 
 		fmt.Println("    Part2:", s.part2function())
 		fmt.Println("")
