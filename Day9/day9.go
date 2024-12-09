332a385f045352429b23308636140c98d54afaf9
Jonas Seiler <Me@jonasseiler.de>
2024-12-09 15:49:27 +0100

    clean  up day9

--- Day9/day9.go
+++ Day9/day9.go
@@ -1,7 +1,6 @@
 package day9
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
@@ -24,46 +23,37 @@ func parseInput() []int {
 	currPos := 0
 
 	for _, n := range sizes {
-		if isFreeSpace {
-			for i := range n {
-				disk[currPos+i] = -1
-			}
-			currPos += n
-		} else {
-			for i := range n {
-				disk[currPos+i] = id
-			}
-			currPos += n
+		entry := -1
+		if !isFreeSpace {
+			entry = id
+		}
+
+		for i := range n {
+			disk[currPos+i] = entry
+		}
+
+		currPos += n
+
+		if !isFreeSpace {
 			id++
 		}
 
 		isFreeSpace = !isFreeSpace
 	}
 
-	//fmt.Println(disk)
-
 	return disk
 }
 
 func Part1() string {
 	disk := parseInput()
+	index := len(disk) - 1
+	i := 0
 
-	left := 0
-	right := len(disk) - 1
-
-	for left < right && right > 0 {
-		if disk[right] != -1 {
-			for disk[left] != -1 && left < len(disk)-1 {
-				left++
-			}
-
-			if left < right {
-				disk[left] = disk[right]
-				disk[right] = -1
-			}
+	for isInBounds(disk, index) && index >= i {
+		if !isFreeSpace(disk, index) {
+			disk, i = moveFileWithStartValue(disk, index, i)
 		}
-
-		right--
+		index--
 	}
 
 	return strconv.Itoa(calculateChecksum(disk))
@@ -71,44 +61,16 @@ func Part1() string {
 
 func Part2() string {
 	disk := parseInput()
-
-	fmt.Println(disk)
-
 	index := len(disk) - 1
 
 	for isInBounds(disk, index) {
-		size := 1
+		length := reverseLengthOfStrip(disk, index)
 
 		if !isFreeSpace(disk, index) {
-			//fmt.Println("test")
-			id := disk[index]
-
-			size, _ = findLengthOfStrip(disk, id, index)
-			//fmt.Println("id", id, "size", size)
-
-			disk = moveFile(disk, id, size, index)
-			//fmt.Println("disk:", disk)
-			//disk[right] = -1
-
-			/*
-				for right-blockSize >= 0 {
-					if disk[right-blockSize] == id {
-						//disk[right-blockSize] = -1
-						blockSize++
-					} else {
-						break
-					}
-				}
-
-				fmt.Println("disk:", disk)
-				fmt.Println("moving block of", id, "with size", blockSize)
-				disk = moveFile(disk, id, blockSize, right-blockSize)
-			*/
+			disk, _ = moveFile(disk, index-(length-1))
 		}
-		index -= size
+		index -= length
 	}
-
-	fmt.Println(disk)
 
 	return strconv.Itoa(calculateChecksum(disk))
 }
@@ -126,61 +88,26 @@ func calculateChecksum(l []int) int {
 	return total
 }
 
-func moveFile(l []int, id int, size int, index int) []int {
+func moveFile(l []int, index int) ([]int, int) {
+	return moveFileWithStartValue(l, index, 0)
+}
 
-	i := findFreeSpace(l, size)
+func moveFileWithStartValue(l []int, index int, start int) ([]int, int) {
+	length := lengthOfStrip(l, index)
+	id := l[index]
 
-	//fmt.Println("free space a")
+	i := findFreeSpace(l, length, start)
 
-	if i >= index {
-		return l
+	if i >= index || i == -1 {
+		return l, start
 	}
 
-	//fmt.Println("test3")
-
-	if i == -1 {
-		return l
-	}
-
-	for j := range size {
+	for j := range length {
 		l[i+j] = id
-		l[index-j] = -1
+		l[index+j] = -1
 	}
 
-	return l
-
-	/*
-
-		for index := range l {
-			if l[index] == -1 {
-				freeSpaceSize := 1
-
-				for freeSpaceSize < size {
-					if index+freeSpaceSize < len(l) {
-						if l[index+freeSpaceSize] == -1 {
-							freeSpaceSize++
-						} else {
-							break
-						}
-					} else {
-						break
-					}
-				}
-
-				if freeSpaceSize < size {
-					continue
-				}
-
-				for i := range size {
-					l[index+i] = id
-				}
-
-				break
-			}
-		}
-
-		return l
-	*/
+	return l, i + length
 }
 
 func isFreeSpace(l []int, index int) bool {
@@ -191,40 +118,38 @@ func isInBounds(l []int, index int) bool {
 	return 0 <= index && index < len(l)
 }
 
-func findLengthOfStrip(l []int, id int, index int) (int, int) {
-	size := 1
+func reverseLengthOfStrip(l []int, index int) int {
+	id := l[index]
+	length := 1
 
-	for isInBounds(l, index-size) && l[index-size] == id {
-		size++
+	for isInBounds(l, index-length) && l[index-length] == id {
+		length++
 	}
 
-	return size, index - (size - 1)
+	return length
 }
 
-func findLengthOfStripR(l []int, id int, index int) (int, int) {
-	size := 1
-	for isInBounds(l, index+size) && l[index+size] == id {
-		size++
+func lengthOfStrip(l []int, index int) int {
+	id := l[index]
+	length := 1
+
+	for isInBounds(l, index+length) && l[index+length] == id {
+		length++
 	}
 
-	return size, index + (size - 1)
+	return length
 }
 
-func findFreeSpace(l []int, size int) int {
-	i := 0
+func findFreeSpace(l []int, size int, start int) int {
+	i := start
 	for isInBounds(l, i) {
-		if l[i] == -1 {
-			length, _ := findLengthOfStripR(l, -1, i)
-			//fmt.Println("at index", i, "found free space of size", length)
+		length := lengthOfStrip(l, i)
 
-			if length >= size {
-				return i
-			} else {
-				i += length
-			}
-		} else {
-			i++
+		if l[i] == -1 && length >= size {
+			return i
 		}
+
+		i += length
 	}
 
 	return -1
