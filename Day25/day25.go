170ca4c2f22245fd52631c8c6fc3eecf4f26175d
Jonas Seiler <Me@jonasseiler.de>
2024-12-29 20:17:10 +0100

    add comment for Day 25

--- Day25/day25.go
+++ Day25/day25.go
@@ -7,6 +7,8 @@ import (
 	"github.com/HerrPixel/AdventOfCode2024/Tools"
 )
 
+// Encodes each key/lock as a list of 5 integers between 0 and 7
+// A key and lock then fit if the sum of the two lists is <= 7 on each index
 func KeyLockCombinations() string {
 	keys := make([][5]int, 0)
 	locks := make([][5]int, 0)
@@ -51,6 +53,7 @@ func KeyLockCombinations() string {
 	return strconv.Itoa(total)
 }
 
+// Just press the button on the site
 func MerryChristmas() string {
 	return "Merry Christmas"
 }
